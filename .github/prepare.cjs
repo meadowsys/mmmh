e9d4d0f8b89a999cffba1e916b9f12537288d724
Alexander Liu <liujalexander84@gmail.com>
2021-05-15 16:07:23 -0700

    init

---
+++ .github/prepare.cjs
@@ -0,0 +1,9 @@
+// @ts-check
+
+const { readFileSync, writeFileSync } = require("fs");
+const { resolve } = require("path");
+
+const pkg = require("../package.json");
+pkg.version = process.env.VERSION;
+
+writeFileSync(resolve(__dirname, "../package.json"), Buffer.from(JSON.stringify(pkg, null, "   ")));
