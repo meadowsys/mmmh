ddd733e98c19ec34857451fa334f20e22693f8a7
Autumn Meadow <blazeykirin@gmail.com>
2022-05-18 00:15:11 -0700

    add basic debounce function

---
+++ src/debounce.ts
@@ -0,0 +1,26 @@
+// heavily took inspiration from underscore
+// https://underscorejs.org
+
+export default function debounce(
+	fn: (this: void) => void,
+	wait: number,
+	immediate = false
+): (this: void) => void {
+	let timeout: ReturnType<typeof setTimeout> | null = null;
+
+	function call() {
+		timeout = null;
+		if (!immediate) fn();
+	}
+
+	return function(...args) {
+		if (timeout) {
+			clearTimeout(timeout);
+			timeout = setTimeout(call, wait);
+			return;
+		}
+
+		if (immediate) fn();
+		timeout = setTimeout(call, wait);
+	}
+}
