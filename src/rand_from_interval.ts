1e901bbccc03767402725d6569c971926c91c852
Autumn Meadow <blazeykirin@gmail.com>
2022-05-18 21:40:29 -0700

    i did an extremely derpy with staging lines

--- src/rand_from_interval.ts
+++ src/rand_from_interval.ts
@@ -1,4 +1,3 @@
-function rand_from_interval(low: number, high: number, round = true) {
 function rand_from_interval(low: number, high: number, round = false) {
 	const num = (Math.random() * Math.abs(high - low)) + low;
 	return round ? Math.floor(num) : num;
